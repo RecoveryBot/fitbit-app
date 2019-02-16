9ada6489ed4877e1e7c6af99e16d25b7272dc76b
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2019-02-16 11:19:17 -0800

    Send heart rate to the server

--- companion/index.js
+++ companion/index.js
@@ -10,15 +10,35 @@ import { settingsStorage } from 'settings';
 let userId = JSON.parse(settingsStorage.getItem('email')).name;
 
 peerSocket.onmessage = event => {
-  console.log(event.data);
-  console.log(userId);
+  const heartRate = event.data;
+  console.log('Heart Rate: ' + heartRate);
+  console.log('User ID: ' + userId);
+  if (userId) {
+    sendToServer(userId, heartRate);
+  }
 };
 
 /*
-  Settings Change
+  Settings
 */
 settingsStorage.onchange = event => {
   if (event.key === 'email') {
     userId = JSON.parse(event.newValue).name;
   }
 }
+
+/*
+  POST to Server
+*/
+const url = 'https://treehacks2019-server.azurewebsites.net';
+const sendToServer = (userId, heartRate) => {
+  return fetch(`${url}/bpm?userId=${userId}&heartRate=${heartRate}`, {
+    method: 'POST',
+    mode: 'cors',
+    credentials: 'omit'
+  })
+  .then(res => {
+    console.log('Sent to server.');
+  })
+  .catch(err => console.error(err));
+};
