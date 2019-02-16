b06f0381037302a769ae98f302b75213cb642331
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2019-02-16 00:36:07 -0800

    Display heart rate

--- app/index.js
+++ app/index.js
@@ -1,1 +1,28 @@
-console.log('Hello world!');
+/*
+  Modules
+*/
+import { HeartRateSensor } from 'heart-rate';
+import document from 'document';
+
+/*
+  Setup
+*/
+const hrm = new HeartRateSensor();
+hrm.start();
+
+/*
+  UI
+*/
+const hrmText = document.getElementById('hrm-data');
+
+/*
+  Main
+*/
+function main() {
+  const heartRate = hrm.heartRate ? hrm.heartRate : 0;
+
+  hrmText.text = JSON.stringify(heartRate);
+}
+
+main();
+setInterval(main, 1000);
