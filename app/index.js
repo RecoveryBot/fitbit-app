03305661c28cdc258ca3d654b6c1cbc95af225c6
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2019-02-16 01:36:57 -0800

    Send heart rate to companion

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@
   Modules
 */
 import { HeartRateSensor } from 'heart-rate';
+import { peerSocket } from 'messaging';
 import document from 'document';
 
 /*
@@ -22,6 +23,11 @@ function main() {
   const heartRate = hrm.heartRate ? hrm.heartRate : 0;
 
   hrmText.text = JSON.stringify(heartRate);
+
+  if (peerSocket.readyState === peerSocket.OPEN) {
+    // Send the data to peer as a message.
+    peerSocket.send(heartRate);
+  }
 }
 
 main();
--- companion/index.js
+++ companion/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+/*
+  Modules
+*/
+import { peerSocket } from 'messaging';
+
+/*
+  Messaging
+*/
+peerSocket.onmessage = event => {
+  console.log(event.data);
+};
