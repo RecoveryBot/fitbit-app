d3742bd0263571dce5316629d806783bddd998ef
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2019-02-16 10:05:12 -0800

    Add alert when heart rate is above threshold

--- app/index.js
+++ app/index.js
@@ -1,8 +1,14 @@
 /*
+  Settings
+*/
+const ALERT_THRESHOLD = 85;
+
+/*
   Modules
 */
 import { HeartRateSensor } from 'heart-rate';
 import { peerSocket } from 'messaging';
+import { vibration } from 'haptics';
 import document from 'document';
 
 /*
@@ -14,12 +20,16 @@ hrm.start();
 /*
   UI
 */
+const body = document.getElementById('body');
 const hrmText = document.getElementById('hrm-data');
 
 /*
   Main
 */
 function main() {
+  body.style.fill = "black";
+  vibration.stop();
+
   const heartRate = hrm.heartRate ? hrm.heartRate : 0;
 
   hrmText.text = JSON.stringify(heartRate);
@@ -27,6 +37,11 @@ function main() {
   if (peerSocket.readyState === peerSocket.OPEN) {
     // Send the data to peer as a message.
     peerSocket.send(heartRate);
+  }
+
+  if (heartRate > ALERT_THRESHOLD) {
+    body.style.fill = "red";
+    vibration.start('alert');
   }
 }
 
