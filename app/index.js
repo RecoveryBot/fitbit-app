53f4a9409ed5075548aeb2f66f2df7486d55272d
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2019-02-17 01:22:45 -0800

    Add alert flow

--- app/index.js
+++ app/index.js
@@ -27,6 +27,14 @@ const hrmLabel = document.getElementById('hrm-label');
 const hrmText = document.getElementById('hrm-data');
 const alert = document.getElementById('alert');
 const alertButtons = document.getElementById('alert-buttons');
+const yesButton = document.getElementById('btn-y');
+const noButton = document.getElementById('btn-n');
+
+/*
+  Globals
+*/
+let alertNo = 0;
+let alertStop = 0;
 
 /*
   Main
@@ -42,7 +50,11 @@ function main() {
   }
 
   if (heartRate > ALERT_THRESHOLD) {
-    showAlert1();
+    if (alertStop === 0) {
+      showAlert();
+    } else {
+      alertStop -= 1000;
+    }
   } else {
     showInitial();
   }
@@ -56,10 +68,11 @@ function showInitial() {
   vibration.stop();
 }
 
-function showAlert1() {
+function showAlert() {
+  if (alertNo !== 2) alertNo = 1;
   setHeader('hidden');
   moveHrm(48);
-  alert.text = 'Your heart rate has dramatically increased. Are you okay?';
+  alert.text = alertNo === 1 ? 'Your heart rate has dramatically increased. Are you okay?' : 'Would you like to reach one of your contacts?';
   setAlert('visible');
   body.style.fill = 'tomato';
   vibration.start('alert');
@@ -79,6 +92,28 @@ function moveHrm(posY) {
   hrmLabel.y = posY;
   hrmText.y = posY + 24;
 }
+
+function disableAlert() {
+  alertStop = 10000;
+  showInitial();
+}
+
+yesButton.onclick = event => {
+  if (alertNo === 1) {
+    alertNo = 0;
+    disableAlert();
+  } else if (alertNo === 2) {
+  }
+};
+
+noButton.onclick = event => {
+  if (alertNo === 1) {
+    alertNo = 2;
+  } else if (alertNo === 2) {
+    alertNo = 0;
+    disableAlert();
+  }
+};
 
 main();
 setInterval(main, 1000);
